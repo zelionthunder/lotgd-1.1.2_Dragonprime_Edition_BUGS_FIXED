1955fd44cf094eae8dc46ccabf8c5325fe3a9534
zelionthunder <zelionthunder@yahoo.com>
2026-07-31 23:13:49 -0500

    Create MySQLi procedural wrapper for PHP 8 compatibility

--- lib/dbwrapper_mysqli_proc.php
+++ lib/dbwrapper_mysqli_proc.php
@@ -1,203 +1,195 @@
 <?php
-// addnews ready
-// translator ready
-// mail ready
+// PHP 8 Compatible MySQLi Procedural Database Wrapper
+// This replaces the deprecated mysql_* functions with MySQLi equivalents
 
-function db_query($sql, $die=true){
- 	if (defined("DB_NODB") && !defined("LINK")) return array();
-	global $session,$dbinfo,$mysqli_resource;
-	$dbinfo['queriesthishit']++;
-	// $fname = DBTYPE."_query";
-	$starttime = getmicrotime();
-	//$r = $fname($sql);
-	$r = mysqli_Query($mysqli_resource, $sql);
+/**
+ * Global connection variable for MySQLi
+ */
+global $mysqli_connection;
 
-	if (!$r && $die === true) {
-	 	if (defined("IS_INSTALLER")){
-	 		return array();
-		}else{
-			if ($session['user']['superuser'] & SU_DEVELOPER || 1){
-				require_once("lib/show_backtrace.php");
-				die(
-					"<pre>".HTMLEntities($sql, ENT_COMPAT, getsetting("charset", "ISO-8859-1"))."</pre>"
-					.db_error(LINK)
-					.show_backtrace()
-					);
-			}else{
-				die("A most bogus error has occurred.  I apologise, but the page you were trying to access is broken.  Please use your browser's back button and try again.");
-			}
-		}
+/**
+ * Connect to MySQL database using MySQLi
+ * @param string $host Database host
+ * @param string $user Database user
+ * @param string $pass Database password
+ * @return mysqli|false Connection object or false on failure
+ */
+function mysqli_proc_connect($host, $user, $pass) {
+	global $mysqli_connection;
+	
+	$mysqli_connection = new mysqli($host, $user, $pass);
+	
+	if ($mysqli_connection->connect_error) {
+		return false;
 	}
-	$endtime = getmicrotime();
-	if ($endtime - $starttime >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT)){
-		$s = trim($sql);
-		if (strlen($s) > 800) $s = substr($s,0,400)." ... ".substr($s,strlen($s)-400);
-		debug("Slow Query (".round($endtime-$starttime,2)."s): ".(HTMLEntities($s, ENT_COMPAT, getsetting("charset", "ISO-8859-1")))."`n");
+	
+	// Set charset to UTF-8
+	$mysqli_connection->set_charset("utf8mb4");
+	
+	return $mysqli_connection;
+}
+
+/**
+ * Persistent connection (same as regular for MySQLi)
+ */
+function mysqli_proc_pconnect($host, $user, $pass) {
+	return mysqli_proc_connect($host, $user, $pass);
+}
+
+/**
+ * Select database
+ * @param string $dbname Database name
+ * @param mysqli|null $link Connection object
+ * @return bool
+ */
+function mysqli_proc_select_db($dbname, $link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
 	}
-	unset($dbinfo['affected_rows']);
-	$dbinfo['affected_rows']=db_affected_rows();
-	$dbinfo['querytime'] += $endtime-$starttime;
-	return $r;
-}
-
-//& at the start returns a reference to the data array.
-//since it's possible this array is large, we'll save ourselves
-//the overhead of duplicating the array, then destroying the old
-//one by returning a reference instead.
-function &db_query_cached($sql,$name,$duration=900){
-	//this function takes advantage of the data caching library to make
-	//all of the other db_functions act just like MySQL queries but rely
-	//instead on disk cached data.
-	//if (getsetting("usedatacache", 0) == 1) debug("DataCache: $name");
-	//standard is 15 minutes, als hooks don't need to be cached *that* often, normally you invalidate the cache properly
-	global $dbinfo;
-	$data = datacache($name,$duration);
-	if (is_array($data)){
-		reset($data);
-		$dbinfo['affected_rows']=-1;
-		return $data;
-	}else{
-		$result = db_query($sql);
-		$data = array();
-		while ($row = db_fetch_assoc($result)) {
-			$data[] = $row;
-		}
-		updatedatacache($name,$data);
-		reset($data);
-		return $data;
+	
+	if (!$link instanceof mysqli) {
+		return false;
 	}
+	
+	return $link->select_db($dbname);
 }
 
-if (file_exists("lib/dbremote.php")) {
-	require_once("lib/dbremote.php");
-}
-
-function db_error(){
-	global $mysqli_resource;
-
-	$r = mysqli_error($mysqli_resource);
- 	if ($r=="" && defined("DB_NODB") && !defined("DB_INSTALLER_STAGE4")) return "The database connection was never established";
-	return $r;
-}
-
-function db_fetch_assoc(&$result){
-	if (is_array($result)){
-		//cached data
-		if (list($key,$val)=each($result))
-			return $val;
-		else
-			return false;
-	}else{
-		$r = mysqli_fetch_assoc($result);
-		return $r;
+/**
+ * Execute a query
+ * @param string $sql SQL query
+ * @param mysqli|null $link Connection object
+ * @return mysqli_result|false
+ */
+function mysqli_proc_query($sql, $link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
 	}
-}
-
-function db_insert_id(){
-  global $mysqli_resource;
- 	if (defined("DB_NODB") && !defined("LINK")) return -1;
-	$r = mysqli_insert_id($mysqli_resource);
-	return $r;
-}
-
-function db_num_rows($result){
-	if (is_array($result)){
-		return count($result);
-	}else{
-	 	if (defined("DB_NODB") && !defined("LINK")) return 0;
-	 	$r = mysqli_num_rows($result);
-		return $r;
+	
+	if (!$link instanceof mysqli) {
+		return false;
 	}
+	
+	return $link->query($sql);
 }
 
-function db_affected_rows($link=false){
-	global $dbinfo, $mysqli_resource;
-	if (isset($dbinfo['affected_rows'])) {
-		return $dbinfo['affected_rows'];
+/**
+ * Get error message
+ * @param mysqli|null $link Connection object
+ * @return string Error message
+ */
+function mysqli_proc_error($link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
 	}
- 	if (defined("DB_NODB") && !defined("LINK")) return 0;
-
- 	$r = mysqli_affected_rows($mysqli_resource);
-
-	return $r;
-}
-
-function db_pconnect($host,$user,$pass){
-  global $mysqli_resource;
-
-	$mysqli_resource = mysqli_connect($host, $user, $pass);
-
-	if($mysqli_resource) {
-	  return true;
+	
+	if (!$link instanceof mysqli) {
+		return "No connection";
 	}
-	else {
-	  return false;
+	
+	return $link->error;
+}
+
+/**
+ * Fetch associative array from result
+ * @param mysqli_result $result Query result
+ * @return array|null Associative array or null
+ */
+function mysqli_proc_fetch_assoc($result) {
+	if (!$result instanceof mysqli_result) {
+		return false;
 	}
+	
+	return $result->fetch_assoc();
 }
 
-function db_connect($host,$user,$pass){
-	global $mysqli_resource;
-
-	$mysqli_resource = mysqli_connect($host, $user, $pass);
-
-	if($mysqli_resource) {
-	  return true;
+/**
+ * Get number of rows in result
+ * @param mysqli_result $result Query result
+ * @return int Number of rows
+ */
+function mysqli_proc_num_rows($result) {
+	if (!$result instanceof mysqli_result) {
+		return 0;
 	}
-	else {
-	  return false;
+	
+	return $result->num_rows;
+}
+
+/**
+ * Get last inserted ID
+ * @param mysqli|null $link Connection object
+ * @return int Last insert ID
+ */
+function mysqli_proc_insert_id($link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
 	}
-}
-
-function db_get_server_version() {
-	global $mysqli_resource;
-	return mysqli_get_server_info($mysqli_resource);
-}
-
-function db_select_db($dbname){
-	global $mysqli_resource;
-	$r = mysqli_select_db($mysqli_resource, $dbname);
-	return $r;
-}
-
-function db_free_result($result){
-	if (is_array($result)){
-		//cached data
-		unset($result);
-	}else{
-	 	if (defined("DB_NODB") && !defined("LINK")) return false;
-		mysqli_free_result($result);
-		return true;
+	
+	if (!$link instanceof mysqli) {
+		return 0;
 	}
+	
+	return $link->insert_id;
 }
 
-function db_table_exists($tablename){
-  global $mysqli_resource;
- 	if (defined("DB_NODB") && !defined("LINK")) return false;
-	$exists = $mysqli_resource->Query("SELECT 1 FROM `$tablename` LIMIT 0");
-	if ($exists) return true;
-	return false;
-}
-
-function db_prefix($tablename, $force=false) {
-	global $DB_PREFIX;
-
-	if ($force === false) {
-		$special_prefixes = array();
-
-		// The following file should be used to override or modify the
-		// special_prefixes array to be correct for your site.  Do NOT
-		// do this unles you know EXACTLY what this means to you, your
-		// game, your county, your state, your nation, your planet and
-		// your universe!
-		if (file_exists("prefixes.php")) require_once("prefixes.php");
-
-		$prefix = $DB_PREFIX;
-		if (isset($special_prefixes[$tablename])) {
-			$prefix = $special_prefixes[$tablename];
-		}
-	} else {
-		$prefix = $force;
+/**
+ * Get number of affected rows
+ * @param mysqli|null $link Connection object
+ * @return int Affected rows count
+ */
+function mysqli_proc_affected_rows($link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
 	}
-	return $prefix . $tablename;
+	
+	if (!$link instanceof mysqli) {
+		return 0;
+	}
+	
+	return $link->affected_rows;
 }
+
+/**
+ * Free result set
+ * @param mysqli_result $result Query result
+ * @return bool
+ */
+function mysqli_proc_free_result($result) {
+	if (!$result instanceof mysqli_result) {
+		return false;
+	}
+	
+	$result->free();
+	return true;
+}
+
+/**
+ * Get server info
+ * @param mysqli|null $link Connection object
+ * @return string Server version
+ */
+function mysqli_proc_get_server_info($link = null) {
+	global $mysqli_connection;
+	
+	if ($link === null) {
+		$link = $mysqli_connection;
+	}
+	
+	if (!$link instanceof mysqli) {
+		return "";
+	}
+	
+	return $link->server_info;
+}
+
 ?>
