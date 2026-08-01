92ece4c2ce456a2d08e8f385cf3b16fe2e8dbff4
zelionthunder <zelionthunder@yahoo.com>
2026-07-31 23:10:11 -0500

    Update dbwrapper.php to default to MySQLi procedural for PHP 8

--- lib/dbwrapper.php
+++ lib/dbwrapper.php
@@ -8,24 +8,30 @@ require_once("lib/datacache.php");
 /* * * *
  * Avaiable values for DBTYPE:
  *
- * - mysql:				The default value. Are you unsure take this.
+ * - mysql:				The default value. Are you unsure take this. (DEPRECATED - removed in PHP 7)
  * - mysqli_oos:		The MySQLi extension of PHP5, object oriented style
- * - mysqli_proc:		The MySQLi extension of PHP5, procedural style
+ * - mysqli_proc:		The MySQLi extension of PHP8+, procedural style (RECOMMENDED)
  *
  */
-define('DBTYPE',"mysql");
+define('DBTYPE',"mysqli_proc");
 
 $dbinfo = array();
 $dbinfo['queriesthishit']=0;
 
 switch(DBTYPE) {
 	case 'mysql':
-		require('lib/dbwrapper_mysql.php');
+		// PHP 8 INCOMPATIBLE - mysql extension was removed in PHP 7.0
+		// For backward compatibility, fall through to mysqli_proc
+		require('lib/dbwrapper_mysqli_proc.php');
 		break;
 	case 'mysqli_oos':
 		require('lib/dbwrapper_mysqli_oos.php');
 		break;
 	case 'mysqli_proc':
+		require('lib/dbwrapper_mysqli_proc.php');
+		break;
+	default:
+		// Default to MySQLi procedural for PHP 8
 		require('lib/dbwrapper_mysqli_proc.php');
 		break;
 }
